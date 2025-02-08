e1570bdce1082cbb84293349e741dc27cabdcb51
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-08 11:19:00 -0800

    Add reference to post in comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_08_185342) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_08_191818) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,5 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_08_185342) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
